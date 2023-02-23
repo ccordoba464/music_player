33e4d3e0a4c369db6295a6032ce4ae8ca0946d7f
ccordoba464 <ccordobaescobar464@gmail.com>
2023-02-23 13:08:49 -0500

    Created UI for Music Player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div className="App">
+      <div className="MusicPlayer">
+        <div className="Display">
+          <div className="Cover">
+            <img
+              src={require("./images/Screenshot 2023-02-07 at 7.49.30 PM.jpg")}
+              alt=""
+            ></img>
+          </div>
+
+          <div className="Information">
+            <h1>
+              <strong>Testing this</strong>
+            </h1>
+            <h2>Rimbo</h2>
+          </div>
+        </div>
+
+        <div className="Controls">
+          <button>Play</button>
+          <button>Pause</button>
+          <button>Skip</button>
+          <button>Rewind</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
